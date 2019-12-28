b577699d6b1c976f52669479947f75e595ad19cc
noglory <marce.ps666@gmail.com>
2019-12-28 18:24:27 -0400

    ListView and ViewHolder.

--- app/src/main/java/com/example/codeswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/codeswag/Controller/MainActivity.kt
@@ -2,12 +2,24 @@ package com.example.codeswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.codeswag.Adapters.CategoryAdapter
+import com.example.codeswag.Model.Category
 import com.example.codeswag.R
+import com.example.codeswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = CategoryAdapter(this,DataService.categories)
+        categoryListView.adapter = adapter
     }
+
+
 }
